28d878cc61ea0acb4491cf1225630cd415e99bda
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2023-03-07 11:49:50 +0100

    Minor changes
    20230307

--- src/components/QuizResult/QuizResult.jsx
+++ src/components/QuizResult/QuizResult.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function QuizResult({ score, maxScore }) {
   return (
     <>
-      Twój wynik: {score}/{maxScore} pkt.
+      <h2>Koniec quizu</h2>
+      <p>
+        Twój wynik: {score}/{maxScore} pkt.
+      </p>
     </>
   );
 }
